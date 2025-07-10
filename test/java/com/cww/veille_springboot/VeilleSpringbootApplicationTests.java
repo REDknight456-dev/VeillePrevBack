c65f6719dab24d0feb1088f327964b2514ee8891
Mohamed Rabie Becha <becha.mohamedrabie@iit.ens.tn>
2025-07-10 08:54:51 +0100

    Add files via upload

---
+++ test/java/com/cww/veille_springboot/VeilleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cww.veille_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeilleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
